6531ce7462278980201e530e97404f78fa574ba7
WGreenwichChaca <walter.greenwich@gmail.com>
2025-03-28 14:03:37 -0500

    modificando rutas

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,30 +1,76 @@
 const pool = require("../db");
 
 const getAllTasks = async (req, res) => {
-  res.send("retrieven a list of task");
+  try {
+    const allTasks = await pool.query("SELECT * FROM task");
+    res.json(allTasks.rows);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getTask = async (req, res) => {
-  res.send("retrieving a single task");
+  try {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    return res.send(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const createTask = async (req, res) => {
   const { title, description } = req.body;
 
-  const result = await pool.query(
-    "INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *",
-    [title, description]
-  );
-
-  res.json(result.rows[0]);
+  try {
+    const result = await pool.query(
+      "INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *",
+      [title, description]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const updateTask = async (req, res) => {
-  res.send("update a task");
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  try {
+    const result = await pool.query(
+      "UPDATE task SET title = $1, description = $2 WHERE id = $3",
+      [title, description, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    return res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const deleteTask = async (req, res) => {
-  res.send("delete a task");
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query("DELETE FROM task WHERE id = $1", [id]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    return res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -10,5 +10,9 @@ app.use(express.json());
 
 app.use(taskRoutes);
 
+app.use((err, req, res, next) => {
+  return res.json({ message: err.message });
+});
+
 app.listen(3000);
 console.log("Server is running on port 3000");
